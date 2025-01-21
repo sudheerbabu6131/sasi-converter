cfc789b34d11f02bb9cd448c3aa735b4376776ce
sudheer <sudheer.barre@sasi.ac.in>
2025-01-21 22:26:47 +0530

    second commit

--- convert.test.js
+++ convert.test.js
@@ -1,12 +1,12 @@
 const convert = require('./convert')
 test('converts 100 INR to USD correctly', () => {
- expect(convert(100)).toBe('1.20');
+ expect(convert(123)).toBe('1.20');
 });
 test('converts 0 INR to USD', () => {
  expect(convert(0)).toBe('0.00');
 });
 test('handles negative values', () => {
- expect(convert(-100)).toBe('-1.20');
+ expect(convert(-123)).toBe('-1.20');
 });
 test('handles non-numerical values', () => {
  expect(()=> convert('ten')).toThrow("Invalid amount");
